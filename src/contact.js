e1ecd27ed198b2961833e876beacc8e5c3c98a71
Ogunyomi Toluwanimi <odinprojectdevs@gmail.com>
2022-08-18 22:40:10 +0000

    completed restaurant page

---
+++ src/contact.js
@@ -0,0 +1,19 @@
+export function makeContact(){
+    const contactDiv = document.createElement('div')
+    contactDiv.classList.add('contact')
+
+    const contactP = document.createElement('p')
+    contactP.innerText = "📞 123 456 789"
+
+    const adressP = document.createElement('p')
+    adressP.innerText = "🏠 Hollywood Boulevard 42, Los Angeles, USA"
+
+    const imageP = document.createElement('img')
+    imageP.src = "../images/restaurant-location.png"
+
+    contactDiv.append(contactP)
+    contactDiv.append(adressP)
+    contactDiv.append(imageP)
+
+    return contactDiv
+}
